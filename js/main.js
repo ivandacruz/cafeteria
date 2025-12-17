b0b3ceacf0bef5c9d934dcf07625bb74100f17a8
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2025-12-17 20:32:09 -0300

    alterações no header

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+var typed = new Typed(".typing",{
+    strings:[
+        "",
+        "Desenvolvedor Fullstack",
+        "Web Developer",
+        "Front-End",
+        "Back-End",
+        "Freelancer"
+    ],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
